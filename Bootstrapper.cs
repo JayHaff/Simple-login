041245b055399164fe293d31cd3bd1be05667cbd
JayHaff <59977836+JayHaff@users.noreply.github.com>
2020-03-21 21:05:54 -0700

    Add files via upload

---
+++ Bootstrapper.cs
@@ -0,0 +1,34 @@
+﻿using Caliburn.Micro;
+using login.Helpers;
+using login.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace login
+{
+    public class Bootstrapper: BootstrapperBase
+    {
+        public Bootstrapper()
+        {
+            Initialize();
+            ConventionManager.AddElementConvention<PasswordBox>(
+           PasswordBoxHelper.BoundPasswordProperty,
+           "Password",
+           "PasswordChanged");
+        }
+
+        protected override void OnStartup(object sender, StartupEventArgs e)
+        {
+            DisplayRootViewFor<ShellViewModel>();
+        }
+
+       
+
+
+    }
+}
