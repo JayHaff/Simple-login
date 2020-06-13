555ca0815e8666350d09295a51bb193d2a4ce9d9
JayHaff <59977836+JayHaff@users.noreply.github.com>
2020-06-12 20:26:01 -0700

    Add files via upload

---
+++ Utils/Aes.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+
+namespace login.Utils
+{
+   public  class Aes
+    {
+        //magicsih/
+        private static RijndaelManaged rijndael = new RijndaelManaged();
+        private static System.Text.UnicodeEncoding unicodeEncoding = new UnicodeEncoding();
+
+        private const int CHUNK_SIZE = 128;
+
+        private void InitializeRijndael()
+        {
+            rijndael.Mode = CipherMode.CBC;
+            rijndael.Padding = PaddingMode.PKCS7;
+        }
+
+        
+        
+        public Aes(String base64key = "gCjK+DZ/GCYbKIGiAt1qCA==", String base64iv = "47l5QsSe1POo31adQ/u7nQ==")
+        {
+            InitializeRijndael();
+
+            rijndael.KeySize = CHUNK_SIZE;
+            rijndael.BlockSize = CHUNK_SIZE;
+
+            rijndael.Key = Convert.FromBase64String(base64key);
+            rijndael.IV = Convert.FromBase64String(base64iv);
+            Console.WriteLine("Here bitch");
+        }
+
+        public Aes(byte[] key, byte[] iv)
+        {
+            InitializeRijndael();
+
+            rijndael.Key = key;
+            rijndael.IV = iv;
+        }
+
+        public string Decrypt(byte[] cipher)
+        {
+            ICryptoTransform transform = rijndael.CreateDecryptor();
+            byte[] decryptedValue = transform.TransformFinalBlock(cipher, 0, cipher.Length);
+            return unicodeEncoding.GetString(decryptedValue);
+        }
+
+        public string DecryptFromBase64String(string base64cipher)
+        {
+            return Decrypt(Convert.FromBase64String(base64cipher));
+        }
+
+        public byte[] EncryptToByte(string plain)
+        {
+            ICryptoTransform encryptor = rijndael.CreateEncryptor();
+            byte[] cipher = unicodeEncoding.GetBytes(plain);
+            byte[] encryptedValue = encryptor.TransformFinalBlock(cipher, 0, cipher.Length);
+            return encryptedValue;
+        }
+
+        public string EncryptToBase64String(string plain)
+        {
+            return Convert.ToBase64String(EncryptToByte(plain));
+        }
+
+        public string GetKey()
+        {
+            return Convert.ToBase64String(rijndael.Key);
+        }
+
+        public string GetIV()
+        {
+            return Convert.ToBase64String(rijndael.IV);
+        }
+
+        public override string ToString()
+        {
+            return "KEY:" + GetKey() + Environment.NewLine + "IV:" + GetIV();
+        }
+    }
+}
---
+++ ViewModels/ShellViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security;
+using System.Text;
+using System.Threading.Tasks;
+using Caliburn.Micro;
+using login.Models;
+using login.Views;
+using System.Windows;
+using System.Windows.Controls;
+using System.Text.RegularExpressions;
+using login.Utils;
+
+namespace login.ViewModels
+{
+    public class ShellViewModel : Conductor<object>
+    {
+
+        public void SignUp()
+        {
+            
+            ActivateItem(new SignUpViewModel());
+        }
+
+        public void SignIn()
+        {
+            ActivateItem(new SignInViewModel());
+        }
+    }
+}
